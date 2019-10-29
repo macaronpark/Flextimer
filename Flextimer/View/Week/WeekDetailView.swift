7b375cac3ec742e1d971329731e95cb4ffc7c373
mararonpark <pxxxsxzy.0@gmail.com>
2019-10-29 21:38:39 +0900

    ➕ workRecordDetailView ing

--- Flextimer/View/Week/WeekDetailView.swift
+++ Flextimer/View/Week/WeekDetailView.swift
@@ -9,13 +9,30 @@
 import SwiftUI
 
 struct WeekDetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+  
+  var record: WorkRecord?
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("출근 시간")) {
+          Text("d")
+        }
+        
+        Section(header: Text("퇴근 시간")) {
+          Text("d")
+        }
+      }
+      .navigationBarTitle(Formatter.dayName.string(from: record?.date ?? Date()))
+      .onAppear { print(self.record?.date) }
     }
+  }
 }
 
 struct WeekDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeekDetailView()
+  static var previews: some View {
+    NavigationView {
+      WeekDetailView(record: WorkRecord())
     }
+  }
 }
