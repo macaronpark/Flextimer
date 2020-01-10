606095d29a9454252dae8b62eb119fd6c8afb6e9
mararonpark <pxxxsxzy.0@gmail.com>
2020-01-11 04:19:43 +0900

    --

--- Flextimer/Presentation/History/History_TableView.swift
+++ Flextimer/Presentation/History/History_TableView.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2020 Suzy Mararon Park. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension HistoryViewController: UITableViewDelegate {
+  
+  func tableView(
+    _ tableView: UITableView,
+    didSelectRowAt
+    indexPath: IndexPath)
+  {
+    
+  }
+}
+
+extension HistoryViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 48
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection
+    section: Int
+  ) -> Int {
+    return 10
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt
+    indexPath: IndexPath) -> UITableViewCell
+  {
+    let cell = tableView.dequeueCell(ofType: HistoryTableViewCell.self, indexPath: indexPath)
+    cell.updateCell()
+    return cell
+  }
+}
--- Flextimer/Presentation/Setting/Setting_TableView.swift
+++ Flextimer/Presentation/Setting/Setting_TableView.swift
@@ -6,4 +6,69 @@
 //  Copyright © 2020 Suzy Mararon Park. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension SettingViewController: UITableViewDelegate {
+  
+  func tableView(
+    _ tableView: UITableView,
+    didSelectRowAt
+    indexPath: IndexPath)
+  {
+    guard let nvc = self.navigationController,
+      let action = self.viewModel.sections[indexPath.section].rows[indexPath.row].action else {
+        return
+    }
+    
+    action(nvc)
+  }
+}
+
+extension SettingViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 48
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return self.viewModel.sections.count
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return self.viewModel.sections[section].title
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection
+    section: Int
+  ) -> Int {
+    return self.viewModel.sections[section].rows.count
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt
+    indexPath: IndexPath) -> UITableViewCell
+  {
+    let row = self.viewModel.sections[indexPath.section].rows[indexPath.row]
+    
+    switch row.component {
+    case .indicator:
+      let cell = tableView.dequeueCell(ofType: SettingBasicCell.self, indexPath: indexPath)
+      cell.accessoryType = .disclosureIndicator
+      cell.updateUI(row)
+      return cell
+      
+    case .label:
+      let cell = tableView.dequeueCell(ofType: SettingLabelCell.self, indexPath: indexPath)
+      cell.updateUI(row)
+      return cell
+      
+    case .none:
+      let cell = tableView.dequeueCell(ofType: SettingBasicCell.self, indexPath: indexPath)
+      cell.updateUI(row)
+      return cell
+    }
+  }
+}
