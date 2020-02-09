c39fb0976aa40fa3e8d062e4834de6944fb43863
mararonpark <pxxxsxzy.0@gmail.com>
2020-02-09 23:26:30 +0900

    Edit textView inset

--- Flextimer/Presentation/History/Memo/DetailMemoViewController.swift
+++ Flextimer/Presentation/History/Memo/DetailMemoViewController.swift
@@ -13,6 +13,7 @@ class DetailMemoViewController: BaseViewController {
   let textView = UITextView().then {
     $0.font = Font.REGULAR_16
     $0.textColor = Color.primaryText
+    $0.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
     $0.isScrollEnabled = true
   }
 
@@ -21,20 +22,19 @@ class DetailMemoViewController: BaseViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    NotificationCenter.default.addObserver(self, selector: #selector(test), name: UIResponder.keyboardWillShowNotification, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(test), name: UIResponder.keyboardWillHideNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(willShowKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(willHideKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
   }
-  
   
   override func setupConstraints() {
     super.setupConstraints()
     
     self.view.addSubview(self.textView)
     self.textView.snp.makeConstraints {
-      $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(16)
-      $0.leading.equalToSuperview().offset(20)
-      $0.trailing.equalToSuperview().offset(-20)
-      $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
+      $0.top.equalTo(self.view.safeAreaLayoutGuide)
+      $0.leading.equalToSuperview()
+      $0.trailing.equalToSuperview()
+      $0.bottom.equalToSuperview()
     }
   }
   
@@ -59,23 +59,22 @@ class DetailMemoViewController: BaseViewController {
     self.doneBarButton.rx.tap.subscribe(onNext: { [weak self] _ in
       self?.view.endEditing(true)
     }).disposed(by: self.disposeBag)
-    
-    
-    
   }
   
-  @objc func test(_ notification: Notification) {
+  @objc func willShowKeyboard(_ notification: Notification) {
+    self.setNavigationBarButtonItem(true)
+    
     let userInfo = notification.userInfo!
-    let keyboardHeight = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue.height
-    let isShow = (notification.name == UIResponder.keyboardWillShowNotification)
-    let offset = isShow ? -(keyboardHeight): 0
-    self.setNavigationBarButtonItem(isShow)
+    let keyboardRect = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+    self.textView.contentInset.bottom = keyboardRect.size.height
+    self.textView.verticalScrollIndicatorInsets.bottom = keyboardRect.size.height
+  }
   
-    DispatchQueue.main.async {
-      self.textView.snp.updateConstraints {
-        $0.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(offset)
-      }
-    }
+  @objc func willHideKeyboard(_ notification: Notification) {
+    self.setNavigationBarButtonItem(false)
+
+    self.textView.contentInset.bottom = 16
+    self.textView.verticalScrollIndicatorInsets.bottom = 16
   }
   
   func setNavigationBarButtonItem(_ isKeyboardShown: Bool) {
