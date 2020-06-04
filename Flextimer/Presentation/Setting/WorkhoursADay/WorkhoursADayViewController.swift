819c781cf467eb735964dd18746c548825ef58db
mararonpark <pxxxsxzy.0@gmail.com>
2020-06-04 23:04:18 +0900

    Remove '60' from min picker component title array

--- Flextimer/Presentation/Setting/WorkhoursADay/WorkhoursADayViewController.swift
+++ Flextimer/Presentation/Setting/WorkhoursADay/WorkhoursADayViewController.swift
@@ -20,7 +20,7 @@ class WorkhoursADayViewController: BaseViewController {
   
   let hours: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   
-  let minutes: [Int] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+  let minutes: [Int] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
   
   var impactGenerator: UIImpactFeedbackGenerator?
   
